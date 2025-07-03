c56003026cffb808e34fbf9fed5edd4779a028d2
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-07-03 14:14:49 +0900

    feat/login-ui

--- Loopy-fe/src/pages/User/Login/index.tsx
+++ Loopy-fe/src/pages/User/Login/index.tsx
@@ -1,8 +1,25 @@
+import CommonInput from "../../../components/Input/CommonInput";
+import LoopyIcon from "../../../assets/images/LoopyIcon.svg?react";
+import CommonButton from "../../../components/button/CommonButton";
+import KakaoLogin from "./_components/KakaoLogin";
+
 const LoginPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#d9d9d9] text-black">
-      <h1 className="text-2xl font-bold">User Page</h1>
-      <p className="mt-2">이 페이지는 유저용입니다.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <LoopyIcon />
+      <div className="w-full pt-[4.375rem]">
+        <CommonInput placeholder="이메일 입력" />
+        <CommonInput placeholder="비밀번호 입력" />
+        <div className="py-[1.5rem]">
+          <CommonButton text="로그인"/>
+        </div>
+        <div className="flex justify-center items-center text-[0.75rem] text-[#7F7F7F]">
+          회원가입
+        </div>
+        <div className="mt-[5.25rem]">
+          <KakaoLogin />
+        </div>
+      </div>
     </div>
   );
 }
