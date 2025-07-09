ccbe78dc2f33e072674b1e4aafddc82bed94a823
hyunsuh <sunnie0704@gmail.com>
2025-07-09 17:25:45 +0900

    feat/challenge-list-page

---
+++ Loopy-fe/src/pages/User/Challenge/mock/mockData.ts
@@ -0,0 +1,83 @@
+export interface ChallengeCardData {
+  challengeMonth?: number;
+  challengeName: string;
+  challengeStartDate: string;
+  challengeDoneDate: string;
+  challengeImage: string;
+  participating: boolean;
+}
+
+export const challengeCardList: ChallengeCardData[] = [
+  {
+    challengeMonth: 7,
+    challengeName: '친구와 함께하는 카공 챌린지',
+    challengeStartDate: '2025.07.01',
+    challengeDoneDate: '2025.07.31',
+    challengeImage:
+      'https://images.unsplash.com/photo-1529070538774-1843cb3265df?auto=format&fit=crop&w=400&q=80',
+    participating: true,
+  },
+  {
+    challengeMonth: 0,
+    challengeName: '친구와 함께하는 카공 챌린지',
+    challengeStartDate: '2025.07.01',
+    challengeDoneDate: '2025.07.31',
+    challengeImage:
+      'https://images.unsplash.com/photo-1529070538774-1843cb3265df?auto=format&fit=crop&w=400&q=80',
+    participating: true,
+  },
+  {
+    challengeMonth: 7,
+    challengeName: '친구와 함께하는 카공 챌린지',
+    challengeStartDate: '2025.07.01',
+    challengeDoneDate: '2025.07.31',
+    challengeImage:
+      'https://images.unsplash.com/photo-1529070538774-1843cb3265df?auto=format&fit=crop&w=400&q=80',
+    participating: false,
+  },
+  {
+    challengeMonth: 0,
+    challengeName: '친구와 함께하는 카공 챌린지',
+    challengeStartDate: '2025.07.01',
+    challengeDoneDate: '2025.07.31',
+    challengeImage:
+      'https://images.unsplash.com/photo-1529070538774-1843cb3265df?auto=format&fit=crop&w=400&q=80',
+    participating: true,
+  },
+  {
+    challengeMonth: 7,
+    challengeName: '친구와 함께하는 카공 챌린지',
+    challengeStartDate: '2025.07.01',
+    challengeDoneDate: '2025.07.31',
+    challengeImage:
+      'https://images.unsplash.com/photo-1529070538774-1843cb3265df?auto=format&fit=crop&w=400&q=80',
+    participating: false,
+  },
+  {
+    challengeMonth: 7,
+    challengeName: '친구와 함께하는 카공 챌린지',
+    challengeStartDate: '2025.07.01',
+    challengeDoneDate: '2025.07.31',
+    challengeImage:
+      'https://images.unsplash.com/photo-1529070538774-1843cb3265df?auto=format&fit=crop&w=400&q=80',
+    participating: false,
+  },
+  {
+    challengeMonth: 7,
+    challengeName: '친구와 함께하는 카공 챌린지',
+    challengeStartDate: '2025.07.01',
+    challengeDoneDate: '2025.07.31',
+    challengeImage:
+      'https://images.unsplash.com/photo-1529070538774-1843cb3265df?auto=format&fit=crop&w=400&q=80',
+    participating: false,
+  },
+  {
+    challengeMonth: 7,
+    challengeName: '친구와 함께하는 카공 챌린지',
+    challengeStartDate: '2025.07.01',
+    challengeDoneDate: '2025.07.31',
+    challengeImage:
+      'https://images.unsplash.com/photo-1529070538774-1843cb3265df?auto=format&fit=crop&w=400&q=80',
+    participating: false,
+  },
+];
