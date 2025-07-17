bbed3d23ccdaedcc4fe64d6c0d477a5110952ad7
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-07-17 18:17:23 +0900

    fix/userLayout

--- Loopy-fe/src/layouts/UserLayout.tsx
+++ Loopy-fe/src/layouts/UserLayout.tsx
@@ -3,7 +3,13 @@ import { Outlet } from 'react-router-dom';
 const UserLayout = () => {
   return (
     <div className="w-full relative flex justify-center font-suit overflow-hidden">
-      <div className="w-full sm:max-w-[393px] min-h-screen bg-white relative flex flex-col px-[1.5rem] outline outline-[0.5px] outline-[#E0E0E0]">
+      {/*sm - 640px 이상
+        md - 768px 이상
+        lg - 1024px 이상
+        xl - 1280px 이상
+        2xl - 1536px 이상
+      */}
+      <div className="w-full xl:max-w-[393px] min-h-screen bg-white relative flex flex-col px-[1.5rem] outline outline-[0.5px] outline-[#E0E0E0]">
         <Outlet />
       </div>
     </div>
