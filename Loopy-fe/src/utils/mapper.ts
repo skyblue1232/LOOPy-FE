10227158ac8fed84b7881092724d5dca6799e716
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-07-25 22:17:54 +0900

    api/signin

---
+++ Loopy-fe/src/utils/mapper.ts
@@ -0,0 +1,18 @@
+import type { SignupRequest } from "../apis/signin/type";
+import type { FormData } from "../types/form";
+
+export const mapFormDataToSignupRequest = (
+  formData: FormData
+): SignupRequest => ({
+  email: formData.email,
+  password: formData.password,
+  nickname: formData.nickname,
+  phoneNumber: formData.phone.replaceAll("-", ""),
+  allowKakaoAlert: true, 
+  agreements: {
+    termsAgreed: true,
+    privacyPolicyAgreed: true,
+    marketingAgreed: false,
+    locationPermission: true,
+  },
+});
