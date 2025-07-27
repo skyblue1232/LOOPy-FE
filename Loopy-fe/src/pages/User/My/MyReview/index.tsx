590076e9d68ccf2ae30dd614764a27f51be9d539
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-07-27 21:06:07 +0900

    feat/my-review-skeleton

--- Loopy-fe/src/pages/User/My/MyReview/index.tsx
+++ Loopy-fe/src/pages/User/My/MyReview/index.tsx
@@ -1,21 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CommonHeader from "../../../../components/header/CommonHeader";
 import ReviewItem from "./_components/ReviewItem";
 import { dummyReviews } from "../../../../mock/dummyReviews";
 import CommonBottomPopup from "../../../../components/popup/CommonBottomPopup";
 import EditReviewPage from "./_components/EditReviewPage";
+import ReviewListSkeleton from "./Skeleton/MyReviewSkeleton";
 
 interface MyReviewPageProps {
   onBack: () => void;
 }
 
 const MyReviewPage = ({ onBack }: MyReviewPageProps) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [reviews, setReviews] = useState(dummyReviews);
   const [showDeleteSuccess, setShowDeleteSuccess] = useState(false);
-  const [editingReviewId, setEditingReviewId] = useState<number | null>(null); 
+  const [editingReviewId, setEditingReviewId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000); 
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleClick = (reviewId: number) => {
-    setEditingReviewId(reviewId); 
+    setEditingReviewId(reviewId);
   };
 
   const handleDelete = (id: number) => {
@@ -36,28 +45,36 @@ const MyReviewPage = ({ onBack }: MyReviewPageProps) => {
   }
 
   return (
-    <div className="flex flex-col -mx-[1.5rem]">
+    <div className="flex flex-col -mx-[1.5rem] custom-scrollbar">
       <div className="mx-[1.5rem]">
         <CommonHeader title="내가 작성한 리뷰" onBack={onBack} />
-        <p className="mt-[1.5rem] text-[1rem] font-semibold">
-          내가 쓴 리뷰 총 {reviews.length}개
-        </p>
       </div>
 
-      <div className="flex-1 overflow-y-auto mt-[1.5rem] space-y-[2.125rem] mb-[3rem]">
-        {reviews.map((review) => (
-          <ReviewItem
-            key={review.id}
-            id={review.id}
-            cafeName={review.cafeName}
-            date={review.date}
-            content={review.content}
-            images={review.images}
-            onClick={() => handleClick(review.id)}
-            onDelete={handleDelete}
-          />
-        ))}
-      </div>
+      {isLoading ? (
+        <ReviewListSkeleton />
+      ) : (
+        <>
+          <div className="mx-[1.5rem]">
+            <p className="mt-[1.5rem] text-[1rem] font-semibold">
+              내가 쓴 리뷰 총 {reviews.length}개
+            </p>
+          </div>
+          <div className="flex-1 overflow-y-auto mt-[1.5rem] space-y-[2.125rem] mb-[3rem]">
+            {reviews.map((review) => (
+              <ReviewItem
+                key={review.id}
+                id={review.id}
+                cafeName={review.cafeName}
+                date={review.date}
+                content={review.content}
+                images={review.images}
+                onClick={() => handleClick(review.id)}
+                onDelete={handleDelete}
+              />
+            ))}
+          </div>
+        </>
+      )}
 
       <CommonBottomPopup
         show={showDeleteSuccess}
