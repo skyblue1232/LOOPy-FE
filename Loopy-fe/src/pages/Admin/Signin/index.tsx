bbc4955430a360a3816b42aafc7c74f2740df53d
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-07-28 07:14:36 +0900

    feat/admin-signin

--- Loopy-fe/src/pages/Admin/Signin/index.tsx
+++ Loopy-fe/src/pages/Admin/Signin/index.tsx
@@ -1,7 +1,30 @@
-const AdminSigninPage = () => {
-  return (
-    <div>index</div>
-  )
-}
+import { useState } from "react";
+import AdminSigninPage from "./_components/AdminSinginPage";
+import type { FormData } from "../../../types/form";
 
-export default AdminSigninPage;
+const AdminSigninPageIndex = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    nickname: "",
+    phone: "",
+    verifyCode: "",
+    agreeTerms: true,
+    agreePrivacy: true,
+  });
+
+  const handleNext = () => {
+    console.log("다음 단계로 이동"); 
+  };
+
+  return (
+    <AdminSigninPage
+      formData={formData}
+      setFormData={setFormData}
+      onNext={handleNext}
+    />
+  );
+};
+
+export default AdminSigninPageIndex;
