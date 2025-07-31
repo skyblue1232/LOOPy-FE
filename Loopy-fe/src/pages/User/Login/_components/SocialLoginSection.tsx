5d2f375d993a88ea7ce91c0bf9151f72df885995
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-07-31 17:44:33 +0900

    fix/kakaoLogin

--- Loopy-fe/src/pages/User/Login/_components/SocialLoginSection.tsx
+++ Loopy-fe/src/pages/User/Login/_components/SocialLoginSection.tsx
@@ -4,32 +4,31 @@ import KakaoLogin from "./KakaoLogin";
 const SocialLoginSection = () => {
   const navigate = useNavigate();
 
+  const handleKakaoLogin = () => {
+    const clientId = import.meta.env.VITE_KAKAO_CLIENT_ID;
+    const redirectBase = import.meta.env.VITE_KAKAO_REDIRECT_URI; 
+
+    const kakaoAuthUrl =
+      `https://kauth.kakao.com/oauth/authorize` +
+      `?client_id=${clientId}` +
+      `&redirect_uri=${encodeURIComponent(redirectBase)}` +
+      `&response_type=code`;
+
+    window.location.href = kakaoAuthUrl;
+  };
+
   return (
     <div className="flex flex-col items-center mt-[3rem]">
       <div className="flex items-center w-full">
         <div className="flex-grow h-px bg-[#DFDFDF] mr-[0.75rem]" />
-        <span className="text-[#DFDFDF] font-semibold text-[0.75rem] whitespace-nowrap">SNS로 로그인하기</span>
+        <span className="text-[#DFDFDF] font-semibold text-[0.75rem] whitespace-nowrap">
+          SNS로 로그인하기
+        </span>
         <div className="flex-grow h-px bg-[#DFDFDF] ml-[0.75rem]" />
       </div>
 
       <div className="my-[1rem] w-full">
-        <KakaoLogin
-          onClick={() => {
-            const clientId = import.meta.env.VITE_KAKAO_CLIENT_ID;
-            const role = "CUSTOMER"; 
-
-            const redirectRawUri = `${import.meta.env.VITE_KAKAO_REDIRECT_URI}?role=${role}`;
-            const redirectUri = encodeURIComponent(redirectRawUri);
-
-            const kakaoAuthUrl =
-              `https://kauth.kakao.com/oauth/authorize` +
-              `?response_type=code` +
-              `&client_id=${clientId}` +
-              `&redirect_uri=${redirectUri}`;
-
-            window.location.href = kakaoAuthUrl;
-          }}
-        />
+        <KakaoLogin onClick={() => handleKakaoLogin()} />
       </div>
 
       <div className="flex justify-center items-center text-[0.75rem] gap-[1rem] mb-[1.25rem]">
