9af8b84474e15bf9179a2e3d4de8fdaa02b7c810
hyunsuh <sunnie0704@gmail.com>
2025-08-02 14:45:28 +0900

    fix/api/stampbook

--- Loopy-fe/src/apis/stampBook/api.ts
+++ Loopy-fe/src/apis/stampBook/api.ts
@@ -6,16 +6,20 @@ export const fetchStampBooks = async (
   sortBy?: 'mostStamped' | 'shortestDeadline',
 ): Promise<StampBook[]> => {
   try {
-    const res = await axiosInstance.get('/api/v1/users/me/stampbooks', {
+    const res = await axiosInstance.get<{
+      resultType: string;
+      error: string | null;
+      success: StampBook[];
+    }>('/api/v1/users/me/stampbooks', {
       params: sortBy ? { sortBy } : {},
     });
 
-    if (!Array.isArray(res.data)) {
-      console.warn('응답이 배열이 아님. mock 반환');
+    if (!Array.isArray(res.data.success)) {
+      console.warn('응답이 배열이 아님. mock 반환', res.data);
       return stampBookListMock;
     }
 
-    return res.data;
+    return res.data.success;
   } catch (e) {
     console.error('API 호출 실패, mock 사용:', e);
     return stampBookListMock;
