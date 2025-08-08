124bd934064804f5bf17dd58e4f8fd4b053410e0
hyunsuh <sunnie0704@gmail.com>
2025-08-09 01:10:53 +0900

    style: notification

--- Loopy-fe/src/pages/Admin/Notification/index.tsx
+++ Loopy-fe/src/pages/Admin/Notification/index.tsx
@@ -12,7 +12,7 @@ const AdminNotificationPage = () => {
         <div className="text-[1.125rem] font-bold leading-none mb-2 mt-6">
           알림 메시지 작성
         </div>
-        <div className="text-[#7F7F7F] text-[0.875rem]">
+        <div className="text-[#7F7F7F] text-[0.875rem] mb-6">
           우리 매장 알림을 설정한 고객님께 메시지를 보내보세요.
         </div>
         <NotificationInput />
