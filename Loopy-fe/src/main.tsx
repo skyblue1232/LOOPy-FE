d16881c44eb5a632d975bd9d826601be0629a1ac
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-08-14 04:35:12 +0900

    fix/build

--- Loopy-fe/src/main.tsx
+++ Loopy-fe/src/main.tsx
@@ -1,10 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { initializeRecaptcha } from "./firebase/initRecaptcha";
 import { cleanUpServiceWorkers } from "./firebase/cleanServiceWorker";
 
-initializeRecaptcha();
 cleanUpServiceWorkers();
 
 createRoot(document.getElementById("root")!).render(
