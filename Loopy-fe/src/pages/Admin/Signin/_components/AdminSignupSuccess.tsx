38f2a51c0b05eb123189fe9d4ae15cb4c126d585
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-08-16 15:08:53 +0900

    fix/sigin-routes

--- Loopy-fe/src/pages/Admin/Signin/_components/AdminSignupSuccess.tsx
+++ Loopy-fe/src/pages/Admin/Signin/_components/AdminSignupSuccess.tsx
@@ -6,7 +6,7 @@ const AdminSignupSuccess = () => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate("/admin/home");
+    navigate("/admin/register");
   };
 
   return (
