377a91a302c1a0ebc3da3ab6d7c7a83f2095da34
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-08-18 21:08:44 +0900

    fix/selectable

--- Loopy-fe/src/components/button/SelectableButton.tsx
+++ Loopy-fe/src/components/button/SelectableButton.tsx
@@ -8,8 +8,8 @@ const SelectableButton = ({ label, selected, onClick }: SelectableButtonProps) =
   return (
     <button
       onClick={onClick}
-      className={`px-[1.25rem] py-[0.625rem] rounded-[50px] text-[0.875rem] font-medium outline-none
-        ${selected ? "bg-[#F0F1FE] text-[#6970F3] border-[0.031rem] border-[#6970F3]" : "text-[#3B3B3B] border-[0.031rem] border-[#A8A8A8]"}`}
+      className={`px-[1.25rem] py-[0.75rem] border-[0.03125rem] rounded-[50px] whitespace-nowrap leading-none text-[0.875rem] font-medium outline-none
+        ${selected ? "bg-[#F0F1FE] text-[#6970F3] border-[#6970F3]" : "text-[#3B3B3B] border-[#A8A8A8]"}`}
     >
       {label}
     </button>
